405b91973ca3d7d7cddcfe2cd8a19f47ce773fde
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-29 19:57:14 -0400

    commit tree bad350b65e93406b24bf6ba021e17db10e9e7283

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,57 @@
-# Make your shoe class here!
+class Shoe 
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+    @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def color=(color)
+        @color = color
+    end 
+
+    def color
+        @color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end
+
+    def material(material)
+        @material = material
+    end
+
+    def material
+        @material
+    end
+
+    def condition(condition)
+        @condition = condition
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+        #shoe.condition = "new"
+     # should rturn the condition of the shoe "new"
+    end
+
+    
+    # shoe.condition = "new"
+    # shoe.cobble
+end
+
+ # Make your shoe class here!
